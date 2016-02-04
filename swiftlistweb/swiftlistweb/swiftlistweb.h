52390f4133f102d1aac87ee8a55630bf93bddf13
crizzwald <toddcrown@icloud.com>
2016-02-04 15:55:48 -0500

    initial commit 2

---
+++ swiftlistweb/swiftlistweb/swiftlistweb.h
@@ -0,0 +1,19 @@
+//
+//  swiftlistweb.h
+//  swiftlistweb
+//
+//  Created by Todd Crown on 1/31/16.
+//  Copyright © 2016 Todd Crown. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swiftlistweb.
+FOUNDATION_EXPORT double swiftlistwebVersionNumber;
+
+//! Project version string for swiftlistweb.
+FOUNDATION_EXPORT const unsigned char swiftlistwebVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftlistweb/PublicHeader.h>
+
+
